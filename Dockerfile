48735f878a2b787952bc039e3b5a2399b6a688e6
mackmoe <maurice.nash1@gmail.com>
2019-08-02 20:33:47 +0000

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM alpine:3.10 as builder
+
+RUN addgroup -g 1000 elasticsearch \
+    && adduser -u 1000 -h /usr/share/elasticsearch -G elasticsearch elasticsearch -D \
+    && apk add --no-cache bash openjdk11-jre-headless su-exec
+
+ENV VERSION=7.2.1
+ENV GPG_KEY=46095ACC8548582C1A2699A9D27D666CD88E42B4
+ENV DOWNLOAD_URL=https://artifacts.elastic.co/downloads/elasticsearch
+ENV EXPECTED_SHA_URL=https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.2.1-linux-x86_64.tar.gz.sha512
+ENV ES_TARBALL_SHA=fa7a3108cc67e09393aea625027e9c3777774994c634d183447900d6e5e420b244319935f9bad43094d6d3ad9f175e5e2065643bf0e2663c6300c99aaf4ab9b0
+ENV ES_TARBAL=https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.2.1-linux-x86_64.tar.gz
+ENV ES_TARBALL_ASC=https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.2.1-linux-x86_64.tar.gz.asc
+
+
+FROM alpine:3.10
+
+ENV OSBUILDER_CONTAINER true
+
+COPY files/bootstrap.sh /tmp/bootstrap.sh
+RUN ["/tmp/bootstrap.sh"]
+
+RUN apk del --purge .build-deps
---
+++ files/bootstrap.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env
+#####
+# This ENV should already be set before this is run but just check to make sure.
+#
+#####
+
+check_workdir() {
+if [ ! $PWD = "/tmp"]; then
+  set -ex
+  cd /tmp
+  echo "===> Install Elasticsearch..."
+else
+  set -ex
+  echo "===> Install Elasticsearch..."
+fi
+}
+
+version_check() {
+if [ "$ES_TARBALL_SHA" ]; then
+  echo "$ES_TARBALL_SHA *elasticsearch.tar.gz" | sha512sum -c -
+fi
+}
+
+add_gpgkeys() {
+if [ "$ES_TARBALL_ASC" ]; then
+  wget --progress=bar:force -O elasticsearch.tar.gz.asc "$ES_TARBALL_ASC"
+  export GNUPGHOME="$(mktemp -d)"
+  gpg --keyserver ha.pool.sks-keyservers.net --recv-keys "$GPG_KEY" || \
+  gpg --keyserver pgp.mit.edu --recv-keys "$GPG_KEY" ||  \
+  gpg --keyserver keyserver.pgp.com --recv-keys "$GPG_KEY" || \
+  gpg --batch --verify elasticsearch.tar.gz.asc elasticsearch.tar.gz
+  rm -rf "$GNUPGHOME" elasticsearch.tar.gz.asc || true
+fi
+}
+
+create_directory_structure() {
+  echo "===> Creating Elasticsearch Paths..."
+  for path in /usr/share/elasticsearch/data /usr/share/elasticsearch/logs /usr/share/elasticsearch/config /usr/share/elasticsearch/config/scripts /usr/share/elasticsearch/tmp /usr/share/elasticsearch/plugins; do
+    mkdir -p "$path";
+    chown -R elasticsearch:elasticsearch "$path";
+  done
+}
+
+untar_elastic() {
+  tar -xf elasticsearch.tar.gz
+  ls -lah
+  mv elasticsearch-$VERSION /usr/share/elasticsearch
+}
+
+check_workdir
+wget --progress=bar:force -O elasticsearch.tar.gz "$ES_TARBAL"
+version_check
+add_gpgkeys
+untar_elastic
+create_directory_structure
+rm -rf /tmp/* /usr/share/elasticsearch/jdk
